23d206ab7bab224da037cf25eb92303a28d952b4
Anthony LLewellyn <drallewellyn@me.com>
2015-09-18 08:10:45 +1000

    add final elements

--- Gemfile
+++ Gemfile
@@ -16,7 +16,6 @@ gem 'devise', '~> 3.5.2'
 gem 'paperclip', '~> 4.3.0'
 gem 'aws-sdk', '< 2.0'
 gem 'masonry-rails'
-gem 'omniauth-twitter', '~> 1.2.1'
 gem 'will_paginate', '~> 3.0.7'
 gem 'will_paginate-bootstrap','~>1.0.1'
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'admin@onthewards.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -259,8 +259,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  #
-  config.omniauth :twitter, "YmVH9f0jV43H1KgSxEi3XV2Yb", "1kayVSpiYudjUdTzhaHiedCxAgNY4vtuMH34ib64P4KasJSOE6"
+  
+
 
 
 end
